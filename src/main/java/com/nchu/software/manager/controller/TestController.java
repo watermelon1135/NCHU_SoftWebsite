8f6fe5d7508b55acc9ba242115990a062eaa8bdb
watermelon1135 <1600965357@qq.com>
2018-12-26 17:15:03 +0800

    init

---
+++ src/main/java/com/nchu/software/manager/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.nchu.software.manager.controller;
+
+import com.nchu.software.manager.domain.TestDoamin;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @ClassName test
+ * @Description: java类作用描述
+ * @Author: watermelon
+ * @CreateDate: 2018/12/25 14:24
+ * @UpdateUser: watermelon
+ * @UpdateDate: 2018/12/25 14:24
+ * @UpdateRemark: 修改内容
+ * @Version: 1.0
+ **/
+@Controller
+public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(TestDoamin t){
+        return t.toString();
+    }
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core" %>
 <html>
 <body>
-<h2>Hello World!</h2>
+
+    <form action="/">
+
+    </form>
 </body>
 </html>
